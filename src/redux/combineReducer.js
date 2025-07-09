d9b5431f07e9cb480612321cd9a5f206666157cb
AliHaider03 <ali.haider342111@gmail.com>
2025-07-09 18:31:41 +0500

    update combine reducer file

--- src/redux/combineReducer.js
+++ src/redux/combineReducer.js
@@ -1,24 +1,35 @@
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+  compose,
+} from "redux";
+import {thunk} from "redux-thunk";
 import apodReducer from "./apod/reducer";
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { thunk } from 'redux-thunk'
 import marsReducer from "./marsRover/reducer";
 import epicReducer from "./epic/reducer";
 import neoReducer from "./neo/reducer";
 import filterReducer from "./filters/reducer";
 
-const middleware = [thunk]
 const reducer = combineReducers({
   apod: apodReducer,
   mars: marsReducer,
   epic: epicReducer,
   neo: neoReducer,
-  filters: filterReducer
+  filters: filterReducer,
 });
+
+const middleware = [thunk];
+
+// ✅ Safe DevTools support in development only
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? require("redux-devtools-extension").composeWithDevTools
+    : compose;
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-)
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
-export default store
+export default store;
